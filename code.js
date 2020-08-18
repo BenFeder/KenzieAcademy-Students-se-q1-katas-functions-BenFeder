7aecbceb3752bd9935052cc4d36a6b9d476d7d8e
Tyler Gary <tygary@gmail.com>
2020-08-18 06:11:25 -0700

    Initial commit

---
+++ code.js
@@ -0,0 +1,38 @@
+/*
+  Katas - Functions
+*/
+
+/*
+------------------------------------------------------
+Add Function
+*/
+
+// Your Code Here.
+
+/*
+------------------------------------------------------
+Multiply Function
+*/
+
+// Your Code Here.
+
+/*
+------------------------------------------------------
+Power/Exponentiation Function
+*/
+
+// Your Code Here.
+
+/*
+------------------------------------------------------
+Factorial Function
+*/
+
+// Your Code Here.
+
+/*
+------------------------------------------------------
+Bonus - Fibonacci Function
+*/
+
+// Your Code Here.
